a02e85bb77e570b084b577075de20670d559568a
charukeshpanjala007 <charukesh.panjala.19.1@mountblue.tech>
2022-03-15 12:36:04 +0530

    Changes in Reduce Function

--- reduce.js
+++ reduce.js
@@ -9,13 +9,16 @@ const reduce = (array, cb = cbFunction, startingValue) => {
     if (Array.isArray(array)) {
         let previousValue = array[0]
         let currentValue
+        let index
         if (startingValue) {
-            previousValue = startingValue  
+            previousValue = startingValue 
+            index = 0 
         }else{
             previousValue = array[0]
+            index = 1
         }
-        for (let index=0; index < array.length-1; index++) {
-            currentValue = array[index+1]
+        for (index; index < array.length; index++) {
+            currentValue = array[index]
             previousValue = cbFunction(previousValue, currentValue)
         }
         return previousValue
